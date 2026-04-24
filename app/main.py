e29f8aedad4e82f27de286da9e2871590951fcf6
Orest <dk.horible@gmail.com>
2026-04-24 19:03:36 +0100

    rework

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init(self, km: int | float) -> None:
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
@@ -12,7 +12,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int | float) -> int:
+    def __add__(self, other: int | float) -> "Distance":
         if isinstance(other, Distance):
             return Distance(self.km + other.km)
         return Distance(self.km + other)
@@ -24,10 +24,10 @@ class Distance:
         self.km += other
         return self
 
-    def __mul__(self, other: "Distance" | int | float) -> "Distance":
+    def __mul__(self, other: int | float) -> "Distance":
         return Distance(self.km * other)
 
-    def __truediv__(self, other: "Distance" | int | float) -> "Distance":
+    def __truediv__(self, other: int | float) -> "Distance":
         return Distance(round(self.km / other , 2))
 
     def __lt__(self, other: "Distance" | int | float) -> bool:
@@ -54,5 +54,3 @@ class Distance:
         if isinstance(other, Distance):
             return self.km >= other.km
         return self.km >= other
-
-
